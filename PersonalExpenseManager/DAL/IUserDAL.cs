980ce8135d7faec1cbcd2da260723f9bc0646ee4
Your Name <tranlebaonghi2105@gmail.com>
2026-05-31 12:42:40 +0700

    Complete Sign Up feature

--- PersonalExpenseManager/DAL/IUserDAL.cs
+++ PersonalExpenseManager/DAL/IUserDAL.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using PersonalExpenseManager.Entity;
+﻿using PersonalExpenseManager.Entity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PersonalExpenseManager.DAL
 {
     internal interface IUserDAL
     {
         List<User> ReadAll();
+
         bool CheckLogin(string username, string password);
+
+        bool Create(User user);
+
+        bool IsUsernameExists(string username);
     }
 }
--- PersonalExpenseManager/DAL/UserDAL.cs
+++ PersonalExpenseManager/DAL/UserDAL.cs
@@ -1,29 +1,82 @@
-﻿using System;
+﻿using PersonalExpenseManager.Entity;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using PersonalExpenseManager.Entity;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PersonalExpenseManager.DAL
 {
     internal class UserDAL : IUserDAL
     {
-        private List<User> users;
+        private readonly string filePath = "users.txt";
+
         public UserDAL()
         {
-            users = new List<User>();
-
-            users.Add(new User("admin", "123456"));
-            users.Add(new User("nguyen", "123456"));
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllLines(filePath, new string[]
+                {
+                    "admin|123456",
+                    "nguyen|123456"
+                });
+            }
         }
+
         public List<User> ReadAll()
         {
+            List<User> users = new List<User>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('|');
+
+                if (parts.Length == 2)
+                {
+                    User user = new User(parts[0], parts[1]);
+                    users.Add(user);
+                }
+            }
+
             return users;
         }
+
         public bool CheckLogin(string username, string password)
         {
-            return users.Any(u => u.Username == username && u.Password == password);
+            username = username.Trim().ToLower();
+            password = password.Trim();
+
+            List<User> users = ReadAll();
+
+            return users.Any(u =>
+                u.Username.Trim().ToLower() == username &&
+                u.Password.Trim() == password
+            );
+        }
+
+        public bool IsUsernameExists(string username)
+        {
+            username = username.Trim().ToLower();
+
+            List<User> users = ReadAll();
+
+            return users.Any(u =>
+                u.Username.Trim().ToLower() == username
+            );
+        }
+
+        public bool Create(User user)
+        {
+            if (IsUsernameExists(user.Username))
+            {
+                return false;
+            }
+
+            string line = user.Username.Trim() + "|" + user.Password.Trim();
+
+            File.AppendAllText(filePath, line + "\n");
+
+            return true;
         }
     }
 }
