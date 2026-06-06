d505d5bc0ce99ce5556c944ff8f4c514df1261d4
hnguyen013 <hanhnguyen130526@gmail.com>
2026-06-06 12:48:32 +0700

    ?

---
+++ PersonalExpenseManager/DAL/ISavingDAL.cs
@@ -0,0 +1,20 @@
+﻿using PersonalExpenseManager.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalExpenseManager.DAL
+{
+    internal interface ISavingDAL
+    {
+        bool Create(Saving s);
+        List<Saving> ReadAll();
+        Saving ReadById(string id);
+        bool Update(Saving s);
+        bool DeleteById(string id);
+        string GetNextId();
+        string GetError();
+    }
+}
---
+++ PersonalExpenseManager/Entity/Saving.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalExpenseManager.Entity
+{
+    internal class Saving
+    {
+        public string Id { get; set; }
+        public string GoalName { get; set; }
+        public double TargetAmount { get; set; }
+        public double SavedAmount { get; set; }
+        public DateTime TargetDate { get; set; }
+        public string Notes { get; set; }
+        public string Status { get; set; }
+
+        public Saving() { }
+
+        public Saving(string id, string goalName, double targetAmount,
+            double savedAmount, DateTime targetDate, string notes, string status)
+        {
+            Id = id;
+            GoalName = goalName;
+            TargetAmount = targetAmount;
+            SavedAmount = savedAmount;
+            TargetDate = targetDate;
+            Notes = notes;
+            Status = status;
+        }
+    }
+}
